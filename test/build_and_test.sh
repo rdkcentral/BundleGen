6ffb68e7e88b811f180d7b1d16e5b621f75f7e17
Stefan Verkoyen <stefan.verkoyen@gmail.com>
2021-05-12 17:03:49 +0200

    Enhance build_and_test scripts
    * add one base script used by the others
    * support docker:// OCI image urls in test script
    * support input DAC tarballs that can be in a subdir
    * add possibility to override libmatching mode,
      target platform and version

---
+++ test/build_and_test.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+
+if [ "$#" -lt 2 ]; then
+    echo "Usage for bundles generated with meta-dac-sdk:"
+    echo "./build_and_test.sh RPI_IPADDRESS DAC-IMAGE.TAR"
+    echo "Examples:"
+    echo "./build_and_test.sh 192.168.1.100 dac-image-wayland-egl-test.tar"
+    echo "./build_and_test.sh 192.168.1.100 dac-image-qt-test.tar"
+    echo ""
+    echo "Usage for existing OCI images:"
+    echo "./build_and_test.sh 192.168.1.100 docker://us.icr.io/appcontainerstagingrdk/you.i:latest sample_app_metadata/youi_reference.json"
+    echo "You might need to login first: skopeo login us.icr.io"
+    echo ""
+    exit
+fi
+
+BOXIP=$1
+APPMETADATA=""
+if [ ! -z "$3" ]; then
+  APPMETADATA="--appmetadata $3"
+fi
+
+## normal, image or host
+if [ -z "$MATCH_MODE" ]; then
+  MATCH_MODE="normal"
+fi
+
+## targets: rpi3, 7218c
+if [ -z "$TARGET" ]; then
+  TARGET="rpi3"
+fi
+
+## target/host platform version, options: morty, dunfell
+if [ -z "$TARGET_VERSION" ]; then
+  TARGET_VERSION="morty"
+fi
+if [ "$TARGET_VERSION" == "morty" ]; then
+  TARGET_VERSION=""
+else
+  TARGET_VERSION="_$TARGET_VERSION"
+fi
+
+if [[ $2 == docker://** ]]; then
+  APP_NAME="testapp"
+  echo "--> Generating runtime bundle..."
+  rm -rf ./${TARGET}-${APP_NAME}
+  ## you might need to login first like: skopeo login us.icr.io
+  bundlegen -vvv generate -m ${MATCH_MODE} --searchpath templates --platform ${TARGET}_reference${TARGET_VERSION} $2 ${TARGET}-${APP_NAME} ${APPMETADATA}
+else
+
+  OCI_TAR=$2
+  # get basename of OCI tar path and remove extension
+  APP_NAME="${OCI_TAR##*/}"
+  APP_NAME="${APP_NAME%.*}"
+
+  if [ ! -f $OCI_TAR ]; then
+    echo "File $OCI_TAR not found!"
+    exit 1
+  fi
+
+  echo "--> Extracting OCI image..."
+  rm -rf ./oci-${APP_NAME}
+  mkdir ./oci-${APP_NAME}
+  tar -xvf $OCI_TAR -C ./oci-${APP_NAME}
+
+  echo "--> Generating runtime bundle..."
+  rm -rf ./${TARGET}-${APP_NAME}
+  bundlegen -vvv generate -m ${MATCH_MODE} --searchpath templates --platform ${TARGET}_reference${TARGET_VERSION} oci:./oci-${APP_NAME}:latest ${TARGET}-${APP_NAME}
+fi
+
+./test/testapp.sh $BOXIP ${TARGET}-${APP_NAME}.tar.gz
