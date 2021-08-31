e89d6b7f790c7945e69bf88165352df77699d2b0
Mallikarjuna Masina <Mallikarjuna.M2@cognizant.com>
2021-08-31 11:54:37 +0530

    BD-963:Compliance Tidy-Up in BundleGen

--- test/build_and_test_on_7218c.sh
+++ test/build_and_test_on_7218c.sh
@@ -1,20 +1,28 @@
 #!/bin/sh
 
-# If not stated otherwise in this file or this component's license file the
-# following copyright and licenses apply:
+##############################################################################
+# Copyright © 2021 Liberty Global B.V. and its Licensors.
+# All rights reserved.
+# Licensed by RDK Management, LLC under the terms of the RDK license.
+# ============================================================================
+# Liberty Global B.V. CONFIDENTIAL AND PROPRIETARY
+# ============================================================================
+# This file (and its contents) are the intellectual property of Liberty Global B.V.
+# It may not be used, copied, distributed or otherwise disclosed in whole or in
+# part without the express written permission of Liberty Global B.V.
+# The RDK License agreement constitutes express written consent by Liberty Global.
+# ============================================================================
+# This software is the confidential and proprietary information of Liberty Global B.V.
+# ("Confidential Information"). You shall not disclose this source code or
+# such Confidential Information and shall use it only in accordance with the
+# terms of the license agreement you entered into.
 #
-# Copyright 2020 Consult Red
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# LIBERTY GLOBAL B.V. MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE
+# SUITABILITY OF THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT
+# LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+# PARTICULAR PURPOSE, OR NON-INFRINGEMENT. LIBERTY GLOBAL B.V. SHALL NOT BE LIABLE FOR
+# ANY DAMAGES SUFFERED BY LICENSEE NOR SHALL THEY BE RESPONSIBLE AS A RESULT
+# OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
+##############################################################################
 
 TARGET="7218c" TARGET_VERSION="morty" MATCH_MODE="normal" ./test/build_and_test.sh "$@"
