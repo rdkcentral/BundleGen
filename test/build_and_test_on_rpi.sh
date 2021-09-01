9299ff15c04b58806f8b9d4da46aacf79dd8ef50
mallikarjuna54 <59596250+mallikarjuna54@users.noreply.github.com>
2021-09-01 23:01:21 +0530

    Update build_and_test_on_rpi.sh

--- test/build_and_test_on_rpi.sh
+++ test/build_and_test_on_rpi.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright 2021 Liberty Global Service B.V.
+# Copyright 2021 Liberty Global Services B.V.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
