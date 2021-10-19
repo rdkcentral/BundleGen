a108d6de40420ad7d53a2e53488d93c4815fbf15
Stefan Verkoyen <stefan.verkoyen@gmail.com>
2021-10-19 17:24:51 +0200

    Add amlogic AH212 template
    * also works for amlogic Skyworth HP44H

--- test/build_and_test.sh
+++ test/build_and_test.sh
@@ -1,19 +1,5 @@
 #!/bin/sh
 
-# Copyright 2021 Liberty Global Services B.V.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 if [ "$#" -lt 2 ]; then
     echo "Usage for bundles generated with meta-dac-sdk:"
     echo "./build_and_test.sh RPI_IPADDRESS DAC-IMAGE.TAR"
@@ -49,6 +35,8 @@ if [ -z "$TARGET_VERSION" ]; then
   TARGET_VERSION="morty"
 fi
 if [ "$TARGET_VERSION" == "morty" ]; then
+  TARGET_VERSION=""
+elif [ "$TARGET_VERSION" == "" ]; then
   TARGET_VERSION=""
 else
   TARGET_VERSION="_$TARGET_VERSION"
