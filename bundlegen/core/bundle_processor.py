2ef02ed499712b19613993cce19baf8859cec3f6
PriyaPesala <116076033+PriyaPesala@users.noreply.github.com>
2022-10-27 14:41:13 +0530

    Update bundle_processor.py

--- bundlegen/core/bundle_processor.py
+++ bundlegen/core/bundle_processor.py
@@ -23,6 +23,7 @@ import textwrap
 from hashlib import sha256
 from loguru import logger
 from pathlib import Path
+from bundlegen.core.utils import Utils
 from bundlegen.core.library_matching import LibraryMatching
 from bundlegen.core.capabilities import *
 
@@ -804,11 +805,11 @@ class BundleProcessor:
                     # We want to ensure that the same image is used if we upgrade to a new bundle version to persist
                     # app data across upgrades. This will ensure the filename is always the same providing the destination
                     # path (path inside the container) do not change.
-                    
-                    
+                    hash_key = dest_path.encode('ascii')
+                    img_name = sha256(hash_key).hexdigest()
 
                     source_path = os.path.join(
-                        persistent_storage_dir, self.app_metadata['id'], f"{Utils.get_random_string(8)}.img")
+                        persistent_storage_dir, self.app_metadata['id'], f"{img_name}.img")
 
                     loopback_mnt_def = {
                         "destination": dest_path,
@@ -1000,7 +1001,3 @@ class BundleProcessor:
         fullPath = os.path.join(self.rootfs_path, path.lstrip('/'))
 
         logger.debug(f"Creating directory {fullPath}")
-
-        # Create the directory if doesn't exist
-        if not os.path.exists(fullPath):
-            os.makedirs(fullPath, 0o755)
