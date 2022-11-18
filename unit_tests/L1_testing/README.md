f392af97bed0a72475f6c429d26b78390efc46c0
PriyaPesala <116076033+PriyaPesala@users.noreply.github.com>
2022-11-18 20:42:53 +0530

    RDK-35868 [DAC] Add unit tests to BundleGen

--- unit_tests/L1_testing/README.md
+++ unit_tests/L1_testing/README.md
@@ -1,21 +1,3 @@
-```# If not stated otherwise in this file or this component's license file the
-# following copyright and licenses apply:
-#
-# Copyright 2021 RDK Management
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-```
-
 # For L1_testing
 Main objective for L1_testing is checking the api’s functionality in *.py script.
 
@@ -30,16 +12,16 @@ Once installed the python version, setup the pip install
 
 ## How to run L1 test
 Run the L1 test using the run_L1_test.py file.
-###For all L1 test:
+## For all L1 test:
 ```bash
     $cd unit_tests/L1_testing
     $python run_L1_test.py
 ```
-###For individual test:
+## For individual test:
 ```bash
     $cd unit_tests/L1_testing
     $python run_L1_test.py -s classname
-    #Ex: $python run_L1_test.py -s Bundleprocessor
+    Ex: $python run_L1_test.py -s Bundleprocessor
 ```
 
 ## Adding additional testfiles
--- unit_tests/L1_testing/test_files/test_Bundleprocessor_ut.py
+++ unit_tests/L1_testing/test_files/test_Bundleprocessor_ut.py
@@ -22,39 +22,43 @@ import sys
 import unittest
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from unit_tests.L1_testing import getting_test_results
+from unit_tests.L1_testing import get_L1_test_results
 from bundlegen.core.bundle_processor import BundleProcessor
 from loguru import logger
-
-has_failures = []
-
 #This class will test the functionality of API's in bundleprocessor.py file.
 class TestBundleProcessor(unittest.TestCase):
-    failure = unittest.TestCase.failureException
-
-    @property
-    def failureException(self):
-         has_failures.append('fail')
-         return self.failure
-
     def setUp(self):
          logger.debug("Setup")
-         global has_failures
-         has_failures = []
-         getting_test_results.add_test_results.add_tests(self)
+         get_L1_test_results.add_test_results.add_tests(self)
 
     def tearDown(self):
-         logger.debug("Tear down")
-         global has_failures
-         if has_failures:
-             getting_test_results.add_test_results.test_failed(self)
-         else:
-             getting_test_results.add_test_results.test_passed(self)
+        logger.debug("tearDown")
+        if hasattr(self._outcome, 'errors'):
+            # Python 3.4 - 3.10  (These two methods have no side effects)
+            result = self.defaultTestResult()
+            self._feedErrorsToResult(result, self._outcome.errors)
+        else:
+            # Python 3.11+
+            result = self._outcome.result
+        ok = all(test != self for test, text in result.errors + result.failures)
+
+       # Demo output:  (print short info immediately - not important)
+        if ok:
+            logger.debug('\nOK: %s' % (self.id(),))
+            get_L1_test_results.add_test_results.test_passed(self)
+
+        for typ, errors in (('ERROR', result.errors), ('FAIL', result.failures)):
+            for test, text in errors:
+                if test is self:
+                    #  the full traceback is in the variable `text`
+                    msg = [x for x in text.split('\n')[1:]
+                           if not x.startswith(' ')][0]
+                    logger.debug("\n\n%s: %s\n     %s" % (typ, self.id(), msg))
+                    get_L1_test_results.add_test_results.test_failed(self, msg)
 
     @classmethod
     def tearDownClass(self):
-        getting_test_results.add_test_results.end_results(self)
+        get_L1_test_results.add_test_results.end_results(self)
 
     def test_process_oci_version(self):
     #When generate_compliant_config: True then it will parse the value of ociversion as 1.0.2
@@ -131,7 +135,6 @@ class TestBundleProcessor(unittest.TestCase):
         processor.platform_cfg = {
             "hardware": {
                 "graphics": True,
-                "maxRam_disabled": "610M"
             }
         }
         processor.oci_config = {
@@ -195,7 +198,7 @@ class TestBundleProcessor(unittest.TestCase):
         self.assertEqual(processor.oci_config, expected)
         logger.debug("-->Test was Successfully verified")
 
-    def test_process_logging_(self):
+    def test_process_logging1(self):
     #Assigning value "logging":{"mode": ""journald""}
         logger.debug("-->Parsing values inside logging plugin based on the value of mode:journald")
         processor = BundleProcessor()
@@ -341,7 +344,6 @@ class TestBundleProcessor(unittest.TestCase):
         processor.platform_cfg = {
             "hardware": {
             "graphics": True,
-            "maxRam_disabled": "610M"
             },
             "gpu":{
             "extraMounts":[ ],
@@ -390,6 +392,47 @@ class TestBundleProcessor(unittest.TestCase):
         }
         processor.platform_cfg = {
 
+        }
+        processor.oci_config = {
+            "mounts":[]
+        }
+        processor._process_mounts()
+        expected = {
+            "mounts": [
+            {
+            "destination": "/data",
+            "type": "none",
+            "source": "/volumes/testing",
+            "options": [
+                "rbind",
+                "rw"
+                    ]
+                }
+            ]
+        }
+        self.assertEqual(processor.oci_config, expected)
+        logger.debug("-->Test was Successfully verified")
+
+    def test_process_mounts1(self):
+        logger.debug("-->Parsing all values of mounts from app_metadata")
+        #print("Parsing the values to mounts")
+        processor = BundleProcessor()
+        processor.rootfs_path = None
+        processor.createmountpoints = None
+        processor.app_metadata={
+        }
+        processor.platform_cfg = {
+         "mounts": [
+            {
+            "destination": "/data",
+            "type": "none",
+            "source": "/volumes/testing",
+            "options": [
+                "rbind",
+                "rw"
+                    ]
+                }
+            ]
         }
         processor.oci_config = {
             "mounts":[]
@@ -583,6 +626,44 @@ class TestBundleProcessor(unittest.TestCase):
         }
         self.assertEqual(processor.oci_config, expected)
         logger.debug("-->Test was Successfully verified")
+
+    def test_should_generate_compliant_config(self):
+        processor = BundleProcessor()
+        processor.rootfs_path = None
+        processor.createmountpoints = None
+        processor.platform_cfg = {
+            "dobby": {
+            "generateCompliantConfig": True
+            }
+        }
+        actual = processor._should_generate_compliant_config()
+        expected = True
+        self.assertEqual(actual, expected)
+
+    def test_should_generate_compliant_config_false(self):
+        processor = BundleProcessor()
+        processor.rootfs_path = None
+        processor.createmountpoints = None
+        processor.platform_cfg = {
+            "dobby": {
+                "generateCompliantConfig": False
+            }
+        }
+        actual = processor._should_generate_compliant_config()
+        expected = False
+
+    def test_is_mapped_1(self):
+        processor = BundleProcessor()
+        actual = processor._is_mapped(None,0)
+        expected = True
+        self.assertEqual(actual, expected)
+
+    def test_is_mapped_2(self):
+        processor = BundleProcessor()
+        actual = processor._is_mapped(0,None)
+        expected = False
+        self.assertEqual(actual, expected)
+
 
 if __name__ == "__main__":
     unittest.main()
